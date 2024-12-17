8e2d167b03b0c69b5b7c9aa5cea3bc2ee19304fd
Kensley <ke049038@SillyComputer>
2024-12-17 13:14:45 -0500

    Added in new mock pages in the repository

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import EventsList from "./components/EventsList/EventsList";
 import Events from "./pages/Events/events";
 import ProfileEditor from "./pages/ProfileEditor/profileEditor";
+import Profile from "./pages/Profile/profile";
+import AboutMe from "./pages/AboutMe/aboutme";
+import Friends from "./pages/Friends/friends";
 import SignUp from "./pages/Signup/signup";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/home";
@@ -15,8 +18,11 @@ function App() {
         <Route path = "/" element = {<Home/>}/>
         <Route path = "/login" element = {<Login/>}/>
         <Route path = "/signup" element = {<SignUp/>}/>
-        <Route path = "/profile" element = {<ProfileEditor/>}/>
+        <Route path = "/editprofile" element = {<ProfileEditor/>}/>
         <Route path = "/events" element = {<Events/>}/>
+        <Route path = "/friends" element = {<Friends/>}/>
+        <Route path = "/aboutme" element = {<AboutMe/>}/>
+        <Route path = "/mainprofile" element = {<Profile/>}/>
       </Routes>
     </BrowserRouter>
   );
